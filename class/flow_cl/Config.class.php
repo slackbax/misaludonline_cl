9dd81d0c7752b8b9f85035d1fbe96661281c15ea
slackbax <i.munoz.j@gmail.com>
2024-03-01 10:27:39 -0300

    Extracting secret keys

--- class/flow_cl/Config.class.php
+++ class/flow_cl/Config.class.php
@@ -1,11 +1,11 @@
 <?php
 $base_url = 'https://www.misaludonline.cl/scheduling/payment';
-//$url = 'https://www.flow.cl/api';
-$url = 'https://sandbox.flow.cl/api';
-//$api_key = '1F07C593-5EA3-4E23-888A-360LDE9726AA';
-$api_key = '21FEC410-CA1A-4423-8651-319L11D2B2E5';
-//$secret_key = '144ca59a83864d63d178c20e272e1d7181865d3b';
-$secret_key = '0f136faa9fd4fb6201e52a6ec599b22d4c3bbf49';
+//$url = FLOW_URL;
+$url = FLOW_SANDBOX_URL;
+//$api_key = FLOW_API_KEY;
+$api_key = FLOW_SANDBOX_API_KEY;
+//$secret_key = FLOW_SECRET_KEY;
+$secret_key = FLOW_SANDBOX_SECRET_KEY;
 
 $COMMERCE_CONFIG = array(
   "APIKEY" => $api_key,
